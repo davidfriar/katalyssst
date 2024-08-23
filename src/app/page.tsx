66054d6506e20b07aba03714290eb95c7a49d16d
David Friar <david.friar@cognifide.com>
2024-08-23 16:55:46 +0100

    test commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import styles from "./page.module.css"
 export default function Home() {
   return (
     <main className={styles.main}>
-      <p>This is for Kat xxx</p>
+      <p>This is for Kat xxx...</p>
     </main>
   )
 }
